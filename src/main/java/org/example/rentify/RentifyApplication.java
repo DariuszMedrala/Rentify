3617f0835ea3b721d5d119aa3ff3a11b38286689
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2025-04-18 12:00:00 +0200

    feat(docs): Integrate Springdoc OpenAPI for Swagger UI and JWT authentication
    This commit adds Springdoc OpenAPI to provide interactive API documentation via Swagger UI. This enhances API discoverability and allows for easy testing of endpoints

--- src/main/java/org/example/rentify/RentifyApplication.java
+++ src/main/java/org/example/rentify/RentifyApplication.java
@@ -1,9 +1,27 @@
 package org.example.rentify;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "Rentify API", version = "v1", description = "API for Rentify Application"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Bearer token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class RentifyApplication {
 
     public static void main(String[] args) {
