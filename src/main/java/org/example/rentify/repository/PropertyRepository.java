ece86cf49e77da540528309a06e7a6651cde7419
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2025-05-08 13:00:00 +0200

    refactor(repostiroy): Prepare PropertyRepository for Service and Controller integration
    This commit refactors the PropertyRepository to support upcoming PropertyService and PropertyController implementation.

--- src/main/java/org/example/rentify/repository/PropertyRepository.java
+++ src/main/java/org/example/rentify/repository/PropertyRepository.java
@@ -1,10 +1,11 @@
 package org.example.rentify.repository;
-
 import org.example.rentify.entity.Property;
 import org.example.rentify.entity.enums.PropertyType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 /*
  * PropertyRepository interface for managing Property entities.
@@ -16,24 +17,45 @@ public interface PropertyRepository extends JpaRepository<Property, Long> {
      * Finds properties by their availability status.
      *
      * @param availability the availability status of the properties
-     * @return a list of properties with the specified availability status
+     * @param pageable the pagination information
+     * @return a page of properties with the specified availability status
      */
-    List<Property> findByAvailability(Boolean availability);
+    Page<Property> findByAvailability(Boolean availability, Pageable pageable);
 
     /**
-     * Finds properties by the username of their owner.
-     *
-     * @param username the username of the property owner
-     * @return a list of properties owned by the specified user
+     * Deletes a property by its ID.
+     * @param id the ID of the property to delete
      */
-    List<Property> findByOwnerUsername(String username);
+    void deletePropertyById(Long id);
+
 
    /**
      * Finds properties by their type and availability status.
      *
      * @param propertyType the type of the properties
      * @param availability the availability status of the properties
-     * @return a list of properties with the specified type and availability status
+    *  @param pageable the pagination information
+     * @return aa page of properties with the specified type and availability status
      */
-    List<Property> findByPropertyTypeAndAvailability(PropertyType propertyType, Boolean availability);
+    Page<Property> findByPropertyTypeAndAvailability(PropertyType propertyType, Boolean availability, Pageable pageable);
+
+    /**
+     * Finds properties by country, city and availability status.
+     *
+     * @param addressCountry the country of the properties
+     * @param addressCity the city of the properties
+     * @param availability the availability status of the properties
+     * @param pageable the pagination information
+     * @return a page of properties with the specified country, city and availability status
+     */
+    Page<Property> findAllByAddress_CountryAndAddress_CityAndAvailability(String addressCountry, String addressCity, Boolean availability, Pageable pageable);
+
+    /**
+     * Finds a property by its ID.
+     *
+     * @param id the ID of the property to find
+     * @return an Optional containing the property if found, or empty if not found.
+     */
+    Optional<Property> findPropertyById(Long id);
+
 }
