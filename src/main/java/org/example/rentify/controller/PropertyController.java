f08f8a030047b33689c9341e36b7aea6e2d40c2f
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2025-05-08 19:00:00 +0200

    feat(property): Implement PropertyController and PropertyService
    This commit introduces the PropertyController and PropertyService to manage property-related operations.

---
+++ src/main/java/org/example/rentify/controller/PropertyController.java
@@ -0,0 +1,241 @@
+package org.example.rentify.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.example.rentify.dto.request.PropertyRequestDTO;
+import org.example.rentify.dto.response.MessageResponseDTO;
+import org.example.rentify.dto.response.PropertyResponseDTO;
+import org.example.rentify.entity.enums.PropertyType;
+import org.example.rentify.service.PropertyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+
+/*
+ * PropertyController is a REST controller for managing properties in the system.
+ * It provides endpoints for creating, updating, deleting, and retrieving properties.
+ */
+@RestController
+@RequestMapping("/api/properties")
+@Tag(name = "Properties Management", description = "Endpoints for managing properties")
+@SecurityRequirement(name = "bearerAuth")
+public class PropertyController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PropertyController.class);
+
+    private final PropertyService propertyService;
+
+    @Autowired
+    public PropertyController(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
+
+    /**
+     * Retrieves all properties in a paginated format.
+     *
+     * @param pageable the pagination information.
+     * @return a paginated list of properties.
+     */
+    @Operation(summary = "Get all properties", description = "Retrieve a paginated list of all properties")
+    @GetMapping("/all")
+    public ResponseEntity<Page<PropertyResponseDTO>> getAllProperties(@Parameter(
+            name = "pageable",
+            description = "Pageable object containing pagination information",
+            example = "{\"page\": 0, \"size\": 10, \"sort\": \"title,asc\"}") Pageable pageable) {
+        Page<PropertyResponseDTO> propertyResponseDTOPage = propertyService.findAllProperties(pageable);
+        return ResponseEntity.ok(propertyResponseDTOPage);
+    }
+
+    /**
+     * Retrieves a property by its ID.
+     *
+     * @param id the ID of the property to retrieve.
+     * @return the property's details.
+     */
+    @Operation(summary = "Get property with given ID", description = "Retrieve a property DTO from given ID")
+    @GetMapping("/{id}")
+    public ResponseEntity<PropertyResponseDTO> getPropertyById(@Parameter(
+            description = "Property ID", in = ParameterIn.PATH) @PathVariable Long id) {
+
+        PropertyResponseDTO property = propertyService.findPropertyById(id);
+        return ResponseEntity.ok(property);
+    }
+
+    /**
+     * Retrieves all properties with a given availability status.
+     *
+     * @param availability the availability status
+     * @param pageable     the pagination information.
+     * @return a paginated list of properties with the given availability status.
+     */
+    @Operation(summary = "Get all properties with availability status", description = "Retrieve a paginated list of all properties with given availability status")
+    @GetMapping("/availability/{availability}")
+    public ResponseEntity<Page<PropertyResponseDTO>> getAllPropertiesByAvailability(
+            @Parameter(description = "Availability status", in = ParameterIn.PATH) @PathVariable boolean availability,
+            @Parameter(name = "pageable",
+                    description = "Pageable object containing pagination information",
+                    example = "{\"page\": 0, \"size\": 10, \"sort\": \"title,asc\"}") Pageable pageable) {
+
+        Page<PropertyResponseDTO> propertyResponseDTOPage = propertyService.findAllPropertiesByAvailability(availability, pageable);
+        return ResponseEntity.ok(propertyResponseDTOPage);
+    }
+
+    /**
+     * Retrieves all properties with a given country name, city name and availability status.
+     *
+     * @param country      the country name
+     * @param city         the city name
+     * @param availability the availability status
+     * @param pageable     the pagination information.
+     * @return a paginated list of properties with the given country name, city name and availability status.
+     */
+    @Operation(summary = "Get all properties with country, city and availability status", description = "Retrieve a paginated list of all properties with given criteria")
+    @GetMapping("/{city}/{country}/{availability}")
+    public ResponseEntity<Page<PropertyResponseDTO>> getAllPropertiesByCityCountryAvailability(
+            @Parameter(description = "Name of the city", in = ParameterIn.PATH) @PathVariable String city,
+            @Parameter(description = "Name of the country", in = ParameterIn.PATH) @PathVariable String country,
+            @Parameter(description = "Availability status", in = ParameterIn.PATH) @PathVariable boolean availability,
+            @Parameter(name = "pageable",
+                    description = "Pageable object containing pagination information",
+                    example = "{\"page\": 0, \"size\": 10, \"sort\": \"title,asc\"}") Pageable pageable) {
+
+        Page<PropertyResponseDTO> propertyResponseDTOPage = propertyService.findAllPropertiesByAddressCountryAndCityAndAvailability(city, country, availability, pageable);
+        return ResponseEntity.ok(propertyResponseDTOPage);
+    }
+
+    /**
+     * Retrieves all properties with a given property type and availability status.
+     *
+     * @param propertyType the property type
+     * @param availability the availability status
+     * @param pageable     the pagination information.
+     * @return a paginated list of properties with the given property type and availability status.
+     */
+    @Operation(summary = "Get all properties with property type and availability status", description = "Retrieve a paginated list of all properties with given criteria")
+    @GetMapping("/{propertyType}/{availability}")
+    public ResponseEntity<Page<PropertyResponseDTO>> getAllPropertiesByPropertyTypeAvailability(
+            @Parameter(description = "Property type", in = ParameterIn.PATH) @PathVariable PropertyType propertyType,
+            @Parameter(description = "Availability status", in = ParameterIn.PATH) @PathVariable boolean availability,
+            @Parameter(name = "pageable",
+                    description = "Pageable object containing pagination information",
+                    example = "{\"page\": 0, \"size\": 10, \"sort\": \"title,asc\"}") Pageable pageable) {
+
+        Page<PropertyResponseDTO> propertyResponseDTOPage = propertyService.findAllPropertiesByPropertyTypeAndAvailability(propertyType, availability, pageable);
+        return ResponseEntity.ok(propertyResponseDTOPage);
+    }
+
+    /**
+     * Creates a new property and assigns it to the currently authenticated user.
+     *
+     * @param propertyRequestDTO property request DTO containing property details.
+     * @param authentication     the authentication object containing the currently authenticated user.
+     * @return a success message or an error message if creation fails.
+     */
+    @Operation(summary = "Create a new property", description = "Allows authenticated users to list a new property. The owner will be the currently authenticated user.")
+    @PostMapping("/create")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> createProperty(@Parameter(description = "Property request DTO containing property details")
+                                            @Valid @RequestBody PropertyRequestDTO propertyRequestDTO,
+                                            Authentication authentication) {
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String username;
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else if (principal instanceof String) {
+            username = (String) principal;
+        } else {
+            logger.error("Unexpected principal type : {}", principal.getClass().getName());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+        PropertyResponseDTO createdProperty = propertyService.createProperty(propertyRequestDTO, username);
+        return ResponseEntity.ok(new MessageResponseDTO("Property created successfully with ID " + createdProperty.getId() + "."));
+    }
+
+    /**
+     * Deletes a property by its ID if logged-in user is the owner of the property or an admin.
+     *
+     * @param id             ID of the property to delete.
+     * @param authentication Authentication object containing the currently authenticated user.
+     * @return A success message or an error message if deletion fails.
+     */
+    @Operation(summary = "Delete a property", description = "Allows authenticated users to delete a property. Requires ADMIN role or for the user to be deleting their own data.")
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("isAuthenticated() and (hasRole('ADMIN') or @propertyService.isOwner(#id, principal.username))")
+    public ResponseEntity<?> deleteProperty(@Parameter(description = "Property ID", in = ParameterIn.PATH) @PathVariable Long id,
+                                            Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String username;
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else if (principal instanceof String) {
+            username = (String) principal;
+        } else {
+            logger.error("Unexpected principal type of: {}", principal.getClass().getName());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+        if (!propertyService.isOwner(id, username)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new MessageResponseDTO("You are not allowed to delete this property."));
+        }
+        propertyService.deletePropertyById(id);
+        logger.info("Property with ID {} deleted successfully by user '{}'", id, authentication.getName());
+        return ResponseEntity.ok(new MessageResponseDTO("Property with ID " + id + " deleted successfully."));
+    }
+
+    /**
+     * Updates a property by its ID if logged-in user is the owner of the property or an admin.
+     *
+     * @param id             ID of the property to update.
+     * @param authentication Authentication object containing the currently authenticated user.
+     * @param propertyRequestDTO Property request DTO containing property details.
+     * @return A success message or an error message if deletion fails.
+     */
+    @Operation(summary = "Update a property", description = "Allows authenticated users to update a property. Requires ADMIN role or for the user to be updating their own data")
+    @PutMapping("update/{id}")
+    @PreAuthorize("isAuthenticated() and (hasRole('ADMIN') or @propertyService.isOwner(#id, principal.username))")
+    public ResponseEntity<?> updateProperty(@Parameter(description = "Property ID", in = ParameterIn.PATH) @PathVariable Long id,
+                                            Authentication authentication,
+                                            @Parameter(description = "Property request DTO containing property details") @Valid @RequestBody PropertyRequestDTO propertyRequestDTO) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String username;
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else if (principal instanceof String) {
+            username = (String) principal;
+        } else {
+            logger.error("Unexpected principal type: {}", principal.getClass().getName());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+        if (!propertyService.isOwner(id, username)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new MessageResponseDTO("You are not allowed to delete this property."));
+        }
+        propertyService.updateProperty(id, propertyRequestDTO);
+        logger.info("Property with ID {} updated successfully by user '{}'", id, authentication.getName());
+        return ResponseEntity.ok(new MessageResponseDTO("Property with ID " + id + " updated successfully."));
+    }
+}
---
+++ src/main/java/org/example/rentify/service/PropertyService.java
@@ -0,0 +1,260 @@
+package org.example.rentify.service;
+
+import org.example.rentify.dto.request.PropertyRequestDTO;
+import org.example.rentify.dto.response.PropertyResponseDTO;
+import org.example.rentify.entity.Property;
+import org.example.rentify.entity.User;
+import org.example.rentify.entity.enums.PropertyType;
+import org.example.rentify.mapper.PropertyMapper;
+import org.example.rentify.repository.PropertyRepository;
+import org.example.rentify.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+
+/*
+ * PropertyService class for managing properties in the system.
+ * This class provides methods to interact with the PropertyRepository.
+ * It throws specific exception for error conditions, to be handled by the global exception handler.
+ */
+@Service
+public class PropertyService {
+
+    private static final Logger logger = LoggerFactory.getLogger(PropertyService.class);
+
+    private final PropertyRepository propertyRepository;
+    private final PropertyMapper propertyMapper;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public PropertyService(PropertyRepository propertyRepository, PropertyMapper propertyMapper, UserRepository userRepository) {
+        this.propertyRepository = propertyRepository;
+        this.propertyMapper = propertyMapper;
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Retrieves all properties in a paginated format as DTOs.
+     *
+     * @param pageable Pagination information.
+     * @return A page of PropertyResponseDTOs.
+     * @throws ResponseStatusException If the propertiesPage is empty
+     */
+    @Transactional(readOnly = true)
+    public Page<PropertyResponseDTO> findAllProperties(Pageable pageable) {
+        logger.debug("Fetching all properties, page: {}, size: {}", pageable.getPageNumber(), pageable.getPageSize());
+        Page<Property> propertiesPage = propertyRepository.findAll(pageable);
+        if (propertiesPage.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No properties found with the specified criteria.");
+        }
+        return propertiesPage.map(propertyMapper::propertyToPropertyResponseDto);
+    }
+
+    /**
+     * Retrieves all properties with availability status
+     *
+     * @param availability The availability of the property.
+     * @return A page of PropertyResponseDTOs.
+     * @throws ResponseStatusException  If the propertiesPage is empty
+     * @throws IllegalArgumentException If the availability is null.
+     */
+    @Transactional(readOnly = true)
+    public Page<PropertyResponseDTO> findAllPropertiesByAvailability(Boolean availability, Pageable pageable) {
+        logger.debug("Fetching all properties with availability, page: {}, size: {}", pageable.getPageNumber(), pageable.getPageSize());
+        Page<Property> propertiesPage = propertyRepository.findByAvailability(availability, pageable);
+        if (propertiesPage.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No properties found with the specified criteria.");
+        }
+        if (availability == null) {
+            throw new IllegalArgumentException("Availability cannot be null.");
+        }
+        return propertiesPage.map(propertyMapper::propertyToPropertyResponseDto);
+    }
+
+    /**
+     * Retrieves all properties with specific criteria in a paginated format as DTOs.
+     *
+     * @param addressCountry The country of the property address.
+     * @param addressCity    The city of the property address.
+     * @param availability   The availability of the property.
+     * @param pageable       Pagination information.
+     * @return A page of PropertyResponseDTOs.
+     * @throws ResponseStatusException  If the propertiesPage is empty
+     * @throws IllegalArgumentException If the addressCountry or addressCity or availability is null.
+     */
+    @Transactional(readOnly = true)
+    public Page<PropertyResponseDTO> findAllPropertiesByAddressCountryAndCityAndAvailability(String addressCountry, String addressCity, Boolean availability, Pageable pageable) {
+        logger.debug("Fetching all properties with specific criteria, page: {}, size: {}", pageable.getPageNumber(), pageable.getPageSize());
+        Page<Property> propertiesPage = propertyRepository.findAllByAddress_CountryAndAddress_CityAndAvailability(addressCountry, addressCity, availability, pageable);
+        if (propertiesPage.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No properties found with the specified criteria.");
+        }
+        if (addressCountry == null || addressCity == null || availability == null) {
+            throw new IllegalArgumentException("Address country, address city and availability cannot be null");
+        }
+        return propertiesPage.map(propertyMapper::propertyToPropertyResponseDto);
+    }
+
+    /**
+     * Retrieves all properties with specific criteria in a paginated format as DTOs.
+     *
+     * @param propertyType The type of the property.
+     * @param availability The availability of the property.
+     * @param pageable     Pagination information.
+     * @return A page of PropertyResponseDTOs.
+     * @throws ResponseStatusException  If the propertiesPage is empty
+     * @throws IllegalArgumentException If the propertyType or availability is null.
+     */
+    @Transactional(readOnly = true)
+    public Page<PropertyResponseDTO> findAllPropertiesByPropertyTypeAndAvailability(PropertyType propertyType, Boolean availability, Pageable pageable) {
+        logger.debug("Fetching all properties with property type and availability, page: {}, size: {}", pageable.getPageNumber(), pageable.getPageSize());
+        Page<Property> propertiesPage = propertyRepository.findByPropertyTypeAndAvailability(propertyType, availability, pageable);
+        if (propertiesPage.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No properties found with the specified criteria.");
+        }
+        if (propertyType == null || availability == null) {
+            throw new IllegalArgumentException("Property type and availability cannot be null");
+        }
+        return propertiesPage.map(propertyMapper::propertyToPropertyResponseDto);
+    }
+
+    /**
+     * Creates a Property and assigns it to the authenticated user.
+     *
+     * @param propertyRequestDTO    The PropertyRequestDTO containing the property details.
+     * @param authenticatedUsername The username of the authenticated user.
+     * @return The PropertyResponseDTO of the created property.
+     * @throws IllegalArgumentException If the PropertyRequestDTO is null, the title is blank, or the address is null.
+     * @throws ResponseStatusException  If the authenticated user is not found in the database. This should not happen.
+     */
+    @Transactional
+    public PropertyResponseDTO createProperty(PropertyRequestDTO propertyRequestDTO, String authenticatedUsername) {
+        if (propertyRequestDTO == null || !StringUtils.hasText(propertyRequestDTO.getTitle()) || propertyRequestDTO.getAddress() == null) {
+            throw new IllegalArgumentException("Property request DTO or title or address cannot be null for creation.");
+        }
+        logger.info("Attempting to create property: {} by authenticated user: {}", propertyRequestDTO.getTitle(), authenticatedUsername);
+
+        User owner = userRepository.findUserByUsername(authenticatedUsername)
+                .orElseThrow(() -> {
+                    logger.error("Authenticated user '{}' not found in database. This should not happen.", authenticatedUsername);
+                    return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Authenticated user data inconsistent.");
+                });
+
+        Property property = propertyMapper.propertyRequestDtoToProperty(propertyRequestDTO);
+        property.setOwner(owner);
+        property.setCreationDate(LocalDateTime.now());
+        property.setAvailability(propertyRequestDTO.isAvailability());
+
+        Property savedProperty = propertyRepository.save(property);
+        logger.info("Property created successfully with ID: {} for owner: {}", savedProperty.getId(), owner.getUsername());
+        return propertyMapper.propertyToPropertyResponseDto(savedProperty);
+    }
+
+    /**
+     * Deletes a property by its ID.
+     *
+     * @param id The ID of the property to delete.
+     * @throws IllegalArgumentException If the ID is null or not positive.
+     * @throws ResponseStatusException  If the property with the given ID is not found in the database.
+     */
+    @Transactional
+    public void deletePropertyById(Long id) {
+        if (id == null || id <= 0) {
+            throw new IllegalArgumentException("Property ID for deletion must be a positive number.");
+        }
+        logger.info("Attempting to delete property with ID: {}", id);
+        propertyRepository.findById(id)
+                .orElseThrow(() -> {
+                    logger.warn("Property not found with ID: {}", id);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "Property not found with ID: " + id);
+                });
+        propertyRepository.deletePropertyById(id);
+        logger.info("Property deleted successfully with ID: {}", id);
+    }
+
+    /**
+     * Updates a property by its ID.
+     *
+     * @param id                 The ID of the property to update.
+     * @param propertyRequestDTO The PropertyRequestDTO containing the property details.
+     * @throws IllegalArgumentException If the PropertyRequestDTO is null, or the ID is not positive or null
+     * @throws ResponseStatusException  If the property with the given ID is not found in the database.
+     */
+    @Transactional
+    public void updateProperty(Long id, PropertyRequestDTO propertyRequestDTO) {
+        if (id == null || id <= 0) {
+            throw new IllegalArgumentException("Property ID for update must be a positive number.");
+        }
+        if (propertyRequestDTO == null) {
+            throw new IllegalArgumentException("Property request DTO cannot be null for update.");
+        }
+        logger.info("Attempting to update property with ID: {}", id);
+
+        Property propertyToUpdate = propertyRepository.findPropertyById(id)
+                .orElseThrow(() -> {
+                    logger.warn("Property not found with given ID number: {}", id);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "Property not found with ID: " + id);
+                });
+        propertyMapper.updatePropertyFromDto(propertyRequestDTO, propertyToUpdate);
+        Property savedProperty = propertyRepository.save(propertyToUpdate);
+        logger.info("Property with ID {} updated successfully.", savedProperty.getId());
+    }
+
+    /**
+     * Finds a property by its ID.
+     *
+     * @param id The ID of the property to find.
+     * @return The PropertyResponseDTO of the found property.
+     * @throws IllegalArgumentException If the ID is null or not positive.
+     * @throws ResponseStatusException  If the property with the given ID is not found in the database.
+     */
+    @Transactional(readOnly = true)
+    public PropertyResponseDTO findPropertyById(Long id) {
+        if (id == null || id <= 0) {
+            throw new IllegalArgumentException("Property ID must be a positive number.");
+        }
+        logger.debug("Attempting to find property with ID: {}", id);
+        Property property = propertyRepository.findPropertyById(id)
+                .orElseThrow(() -> {
+                    logger.warn("Property not found with provided ID: {}", id);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "Property not found with ID: " + id);
+                });
+        logger.info("Property found successfully with ID: {}", id);
+        return propertyMapper.propertyToPropertyResponseDto(property);
+    }
+
+
+    /**
+     * Checks if a property with a given ID has 'owner' with a given username.
+     *
+     * @param id       The ID of the property to check.
+     * @param username The username of the owner to check.
+     * @throws IllegalArgumentException If the ID or username is null or not positive.
+     * @throws ResponseStatusException  If the property with the given ID is not found in the database, or the owner is not found in the database.
+     * @return True if the property has the given owner, false otherwise.
+     */
+    public boolean isOwner(Long id, String username) {
+        if (id == null || id <= 0 || !StringUtils.hasText(username)) {
+            throw new IllegalArgumentException("Property ID and username must be a positive number and not blank.");
+        }
+        if (userRepository.findUserByUsername(username).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with given username: " + username);
+        }
+        logger.debug("Checking if property with ID: {} has owner with username: {}", id, username);
+        Property property = propertyRepository.findById(id)
+                .orElseThrow(() -> {
+                    logger.warn("Property not found with given ID: {}", id);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "Property not found with ID: " + id);
+                });
+        return property.getOwner().getUsername().equals(username);
+    }
+}
