f549b5c882ff7bf262806f8f632dfd8fcb06d911
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2025-05-12 12:00:00 +0200

    refactor(controller): Enhance ReviewController with improved authorization, API consistency, and validation
    This commit significantly refactors the ReviewController to improve security, streamline request handling, standardize API responses, and introduce input validation.

--- src/main/java/org/example/rentify/controller/ReviewController.java
+++ src/main/java/org/example/rentify/controller/ReviewController.java
@@ -4,10 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.example.rentify.dto.request.ReviewRequestDTO;
+import org.example.rentify.dto.response.MessageResponseDTO;
 import org.example.rentify.dto.response.ReviewResponseDTO;
 import org.example.rentify.service.ReviewService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -36,21 +39,14 @@ public class ReviewController {
      * Retrieves reviews for a specific booking.
      *
      * @param bookingId the ID of the booking
-     * @param authentication the authentication object containing user details
      * @return a ResponseEntity containing the reviews for the specified booking
      */
     @Operation(summary = "Get review by booking ID", description = "Retrieves review for a specific booking.")
     @GetMapping("/{bookingId}")
     @PreAuthorize("isAuthenticated() and (hasRole('ADMIN') or @bookingService.isBookingOwner(#bookingId, principal.username))")
     public ResponseEntity<ReviewResponseDTO> getReviewsFromBookingId(@Parameter(description = "Booking ID", in = ParameterIn.PATH)
-                                                                         @PathVariable Long bookingId,
-                                                                     Authentication authentication) {
-        if (authentication == null || !(authentication.getPrincipal() instanceof UserDetails userDetails)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        String username = userDetails.getUsername();
-        ReviewResponseDTO reviews = reviewService.getReviewsByBookingId(bookingId, username);
-        return ResponseEntity.ok(reviews);
+                                                                         @PathVariable Long bookingId) {
+        return ResponseEntity.ok(reviewService.getReviewsByBookingId(bookingId));
     }
 
     /**
@@ -63,12 +59,9 @@ public class ReviewController {
     @GetMapping("/me/all")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<ReviewResponseDTO>> getAllReviews(Authentication authentication) {
-        if (authentication == null || !(authentication.getPrincipal() instanceof UserDetails userDetails)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         String username = userDetails.getUsername();
-        List<ReviewResponseDTO> reviews = reviewService.getAllReviewsByUser(username);
-        return ResponseEntity.ok(reviews);
+        return ResponseEntity.ok(reviewService.getAllReviewsByUser(username));
     }
 
     /**
@@ -81,51 +74,37 @@ public class ReviewController {
     @GetMapping("/property/{propertyId}")
     public ResponseEntity<List<ReviewResponseDTO>> getAllReviewsByPropertyId(@Parameter(description = "Property ID", in = ParameterIn.PATH)
                                                                                      @PathVariable Long propertyId) {
-        List<ReviewResponseDTO> reviews = reviewService.getAllReviewsByPropertyId(propertyId);
-        return ResponseEntity.ok(reviews);
+        return ResponseEntity.ok(reviewService.getAllReviewsByPropertyId(propertyId));
     }
 
     /**
      * Creates a new review for a specific booking.
      *
      * @param bookingId the ID of the booking
-     * @param reviewRequestDTO the DTO containing review data to be created
-     * @param authentication the authentication object containing user details
-     * @return a ResponseEntity containing the created review
+     * @param reviewRequestDTO the DTO containing review data
+     * @return a MessageResponseDTO containing the message if created successfully
      */
     @Operation(summary = "Create review for booking", description = "Creates a new review for a specific booking.")
     @PostMapping("/{bookingId}/create")
     @PreAuthorize("isAuthenticated() and (hasRole('ADMIN') or @bookingService.isBookingOwner(#bookingId, principal.username))")
-    public ResponseEntity<ReviewResponseDTO> createReview(@Parameter(description = "Booking ID", in = ParameterIn.PATH)
-                                                               @PathVariable Long bookingId,
-                                                          @Parameter(description = "Review Request DTO") @RequestBody ReviewRequestDTO reviewRequestDTO,
-                                                          Authentication authentication) {
-        if (authentication == null || !(authentication.getPrincipal() instanceof UserDetails userDetails)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        String username = userDetails.getUsername();
-        ReviewResponseDTO review = reviewService.createReview(bookingId, reviewRequestDTO, username);
-        return ResponseEntity.status(HttpStatus.CREATED).body(review);
+    public MessageResponseDTO createReview(@Parameter(description = "Booking ID", in = ParameterIn.PATH)
+                                                              @PathVariable Long bookingId,
+                                                          @Parameter(description = "Review Request DTO")
+                                                          @Valid @RequestBody ReviewRequestDTO reviewRequestDTO){
+        return reviewService.createReview(bookingId, reviewRequestDTO);
     }
 
-    /**
+   /**
      * Deletes a review by its ID.
      *
      * @param reviewId the ID of the review to be deleted
-     * @param authentication the authentication object containing user details
-     * @return a ResponseEntity indicating the result of the deletion operation
+     * @return a MessageResponseDTO containing the message if deleted successfully
      */
     @Operation(summary = "Delete review by ID", description = "Deletes a review by its ID.")
     @DeleteMapping("/{reviewId}/delete")
-    @PreAuthorize("isAuthenticated() or hasRole('ADMIN')")
-    public ResponseEntity<?> deleteReview(@Parameter(description = "Review ID", in = ParameterIn.PATH) @PathVariable Long reviewId,
-                                          Authentication authentication) {
-        if (authentication == null || !(authentication.getPrincipal() instanceof UserDetails userDetails)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        String username = userDetails.getUsername();
-        reviewService.deleteReview(reviewId, username);
-        return ResponseEntity.status(HttpStatus.OK).body("Review deleted successfully");
+    @PreAuthorize("isAuthenticated() and (hasRole('ADMIN') or @reviewService.isReviewOwner(#reviewId, principal.username))")
+    public MessageResponseDTO deleteReview(@Parameter(description = "Review ID", in = ParameterIn.PATH) @PathVariable Long reviewId) {
+        return reviewService.deleteReview(reviewId);
     }
 
     /**
@@ -133,42 +112,32 @@ public class ReviewController {
      *
      * @param reviewId the ID of the review to be updated
      * @param reviewRequestDTO the DTO containing updated review data
-     * @param authentication the authentication object containing user details
-     * @return a ResponseEntity containing the updated review
+     * @return a MessageResponseDTO containing the message if updated successfully
      */
     @Operation(summary = "Update review by ID", description = "Updates a review by its ID.")
     @PutMapping("/{reviewId}/update")
-    @PreAuthorize("isAuthenticated() or hasRole('ADMIN')")
-    public ResponseEntity<ReviewResponseDTO> updateReview(@Parameter(description = "Review ID", in = ParameterIn.PATH) @PathVariable Long reviewId,
-                                                          @Parameter(description = "Review Request DTO") @RequestBody ReviewRequestDTO reviewRequestDTO,
-                                                          Authentication authentication) {
-        if (authentication == null || !(authentication.getPrincipal() instanceof UserDetails userDetails)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        String username = userDetails.getUsername();
-        ReviewResponseDTO updatedReview = reviewService.updateReview(reviewId, reviewRequestDTO, username);
-        return ResponseEntity.ok(updatedReview);
+    @PreAuthorize("isAuthenticated() and (hasRole('ADMIN') or @reviewService.isReviewOwner(#reviewId, principal.username))")
+    public MessageResponseDTO updateReview(@Parameter(description = "Review ID", in = ParameterIn.PATH) @PathVariable Long reviewId,
+                                          @Parameter(description = "Review Request DTO")
+                                          @Valid @RequestBody ReviewRequestDTO reviewRequestDTO){
+       return reviewService.updateReview(reviewId, reviewRequestDTO);
     }
+
     /**
-     * Updates the description of a review by its ID.
+     * Updates the comment of a review by its ID.
      *
      * @param reviewId the ID of the review to be updated
-     * @param description the new description for the review
-     * @param authentication the authentication object containing user details
-     * @return a ResponseEntity containing the updated review
+     * @param comment the new comment for the review
+     * @return a MessageResponseDTO containing the message if updated successfully
      */
-    @Operation(summary = "Update review description by ID", description = "Updates a review description by its ID.")
-    @PatchMapping("/{reviewId}/update/description")
-    @PreAuthorize("isAuthenticated() or hasRole('ADMIN')")
-    public ResponseEntity<ReviewResponseDTO> updateReviewDescription(@Parameter(description = "Review ID", in = ParameterIn.PATH) @PathVariable Long reviewId,
-                                                                     @Parameter(description = "Description") @RequestBody String description,
-                                                                      Authentication authentication) {
-        if (authentication == null || !(authentication.getPrincipal() instanceof UserDetails userDetails)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        String username = userDetails.getUsername();
-        ReviewResponseDTO updatedReview = reviewService.updateReviewDescription(reviewId, description, username);
-        return ResponseEntity.ok(updatedReview);
+    @Operation(summary = "Update review comment by ID", description = "Updates a review comment by its ID.")
+    @PatchMapping("/{reviewId}/update/comment")
+    @PreAuthorize("isAuthenticated() and (hasRole('ADMIN') or @reviewService.isReviewOwner(#reviewId, principal.username))")
+    public MessageResponseDTO updateReviewComment (@Parameter(description = "Review ID", in = ParameterIn.PATH) @PathVariable Long reviewId,
+                                                                     @Max(value = 2000, message = "Comment must be below 2000 words")
+                                                                     @Parameter(description = "Comment", in = ParameterIn.PATH)
+                                                                     @RequestBody String comment){
+        return reviewService.updateReviewComment(reviewId, comment);
     }
 
     /**
@@ -176,21 +145,17 @@ public class ReviewController {
      *
      * @param reviewId the ID of the review to be updated
      * @param rating the new rating for the review
-     * @param authentication the authentication object containing user details
-     * @return a ResponseEntity containing the updated review
+     * @return a MessageResponseDTO containing the message if updated successfully
      */
     @Operation(summary = "Update review rating by ID", description = "Updates a review rating by its ID.")
     @PatchMapping("/{reviewId}/update/rating/{rating}")
-    @PreAuthorize("isAuthenticated() or hasRole('ADMIN')")
-    public ResponseEntity<ReviewResponseDTO> updateReviewRating(@Parameter(description = "Review ID", in = ParameterIn.PATH) @PathVariable Long reviewId,
-                                                                 @Parameter(description = "Rating", in = ParameterIn.PATH)  @PathVariable Integer rating,
-                                                                 Authentication authentication) {
-        if (authentication == null || !(authentication.getPrincipal() instanceof UserDetails userDetails)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        String username = userDetails.getUsername();
-        ReviewResponseDTO updatedReview = reviewService.updateReviewRating(reviewId, rating, username);
-        return ResponseEntity.ok(updatedReview);
+    @PreAuthorize("isAuthenticated() and (hasRole('ADMIN') or @reviewService.isReviewOwner(#reviewId, principal.username))")
+    public MessageResponseDTO updateReviewRating(@Parameter(description = "Review ID", in = ParameterIn.PATH)
+                                                                    @PathVariable Long reviewId,
+                                                 @Parameter(description = "Rating", in = ParameterIn.PATH)
+                                                                 @Min(value = 1, message = "Rating must be at least 1.")
+                                                                 @Max(value = 5, message = "Rating must be at most 5.")
+                                                                 @PathVariable Integer rating){
+        return reviewService.updateReviewRating(reviewId, rating);
     }
-
 }
